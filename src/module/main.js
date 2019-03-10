57ce616c306c7ff0e5f7c8fdfaa1b0c545bdfbfe
liuzhu <1764843877@qq.com>
2019-03-10 21:48:44 +0800

    完成首页路路由配置

---
+++ src/module/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render () {
+        return (
+            <div>
+                主页
+            </div>
+        );
+    }
+}
+export default Main;
